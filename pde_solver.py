a5cd79c77dc1da6b668bc8cedf36fdd02cbe920a
Matheus Freitas <matt.g.freitas@gmail.com>
2022-04-11 21:19:56 -0300

    added solution to 1d heat equation

--- pde_solver.py
+++ pde_solver.py
@@ -7,17 +7,42 @@ def solve_wave_equation(
     steps: tuple[float],
     max_time: float,
     type: str,
-    velocity: float = 1.
+    constant: float = 1.,
+    initial_velocities: np.ndarray = None,
 ) -> np.ndarray:
-    length = int(max_time/steps[0])
+    length = int(max_time/steps[1])
     ic_len = len(initial_condition)
+    if initial_velocities is None:
+        initial_velocities = np.zeros([ic_len])
     if type == "free":
         array = pde_solver.solver.free_wave_equation_1d(
             ic=initial_condition,
-            c=velocity,
+            iv=initial_velocities,
+            c=constant,
             sol_len=length,
-            delta_t=steps[1],
             delta_x=steps[0],
+            delta_t=steps[1],
+            ic_len=ic_len,
+        )
+    return array
+
+
+def solve_heat_equation(
+    initial_condition: np.ndarray,
+    steps: tuple[float],
+    max_time: float,
+    type: str,
+    constant: float = 1.
+) -> np.ndarray:
+    length = int(max_time/steps[1])
+    ic_len = len(initial_condition)
+    if type == "free":
+        array = pde_solver.solver.free_heat_equation_1d(
+            ic=initial_condition,
+            c=constant,
+            sol_len=length,
+            delta_x=steps[0],
+            delta_t=steps[1],
             ic_len=ic_len,
         )
     return array
--- solvers/pde_solver.f95
+++ solvers/pde_solver.f95
@@ -2,32 +2,59 @@ module solver
 
   contains
 
-    function free_wave_equation_1d(ic_len, delta_x, delta_t, sol_len, c, ic) result(array)
+    function free_wave_equation_1d(ic_len, delta_x, delta_t, sol_len, c, ic, iv) result(array)
       implicit none
 
       integer, intent(in) :: ic_len, sol_len
-      real, intent(in) :: ic(ic_len), delta_x, delta_t, c
-      real :: array(ic_len, sol_len), acceleration(ic_len), velocity(ic_len, sol_len)
+      real, intent(in) :: ic(ic_len), iv(ic_len), delta_x, delta_t, c
+      real :: array(ic_len, sol_len), acceleration(ic_len)
+      real :: velocity(ic_len, sol_len), constant
       integer :: i, j
 
+      constant = (c**2)/(delta_x**2)
       array(:, 1) = ic
-      velocity = 0
+      velocity(:, 1) = iv
 
       do i = 2, sol_len, 1
 
-        acceleration(1) = (array(2, i-1) - array(1, i-1))*(c/delta_x)
-        acceleration(ic_len) = (array(ic_len-1, i-1) - array(ic_len, i-1))*(c/delta_x)
+        acceleration(1) = ( array(2, i-1) - array(1, i-1) )
+        acceleration(ic_len) = (array(ic_len-1, i-1) - array(ic_len, i-1))
         do j = 2, ic_len-1, 1
-          acceleration(j) = ( array(j-1, i-1) + array(j+1, i-1) &
-            - (2*array(j, i-1)) )*(c**2)/(delta_x**2)
+          acceleration(j) = ( array(j-1, i-1) + array(j+1, i-1) - (2*array(j, i-1)) )
         end do
 
-        acceleration = acceleration
+        acceleration = acceleration*constant
         velocity(:, i) = velocity(:, i-1) + (acceleration*delta_t)
+        write(*,*) velocity(:, i)
         array(:, i) = array(:, i-1) + (velocity(:, i)*delta_t)
 
       end do
 
     end function free_wave_equation_1d
+
+    function free_heat_equation_1d(ic_len, delta_x, delta_t, sol_len, c, ic) result(array)
+      implicit none
+
+      integer, intent(in) :: ic_len, sol_len
+      real, intent(in) :: ic(ic_len), delta_x, delta_t, c
+      real :: array(ic_len, sol_len), acceleration(ic_len), constant
+      integer :: i, j
+
+      constant = delta_t*c/(delta_x**2)
+      array(:, 1) = ic
+
+      do i = 2, sol_len, 1
+
+        acceleration(1) = (array(2, i-1) - array(1, i-1))
+        acceleration(ic_len) = (array(ic_len-1, i-1) - array(ic_len, i-1))
+        do j = 2, ic_len-1, 1
+          acceleration(j) = array(j+1, i-1) - ( 2*array(j, i-1) ) + array(j-1, i-1)
+        end do
+
+        array(:, i) = array(:, i-1) + (acceleration*constant)
+
+      end do
+
+    end function free_heat_equation_1d
 
 end module solver
