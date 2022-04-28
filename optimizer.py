642cedd14a0eb039c773d5a427a11e5a8e4dd424
Matheus Freitas <matt.g.freitas@gmail.com>
2022-04-28 01:55:17 -0300

    more intuitive syntax for simple_fit

--- optimizer.py
+++ optimizer.py
@@ -2,10 +2,10 @@ import simple_numerical_package.fortran_bins.optimizer as optimizer
 import numpy as np
 
 
-def simple_fit(array: np.ndarray, type: str = "linear") -> tuple[np.ndarray]:
+def simple_fit(x: np.ndarray, y: np.ndarray, type: str = "linear") -> tuple[np.ndarray]:
     """
         A simple and fast linear, and log, 1d fit
-        array is an (2, n) shaped array, with x on (0, :) and y on (1, :)
+        x and y are 1d arrays of the same size
         type is the selected fit,
             "linear" is the classical y = a*x + b
             "log" is for functions like y = b * x^a
@@ -13,10 +13,11 @@ def simple_fit(array: np.ndarray, type: str = "linear") -> tuple[np.ndarray]:
     """
     if type == "linear":
         result = optimizer.optimizer.linear_least_squares(
-            matrix=array, n=array.shape[1])
+            x=x, y=y, n=len(x))
     elif type == "log":
-        init_array = np.log(array)
+        new_x = np.log(x)
+        new_y = np.log(y)
         result = optimizer.optimizer.linear_least_squares(
-            matrix=init_array, n=array.shape[1])
+            x=new_x, y=new_y, n=len(x))
         result[:, 1] = np.exp(result[:, 1])
     return result[0], result[1]
